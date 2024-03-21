1dd266eaa6511f167c82f713a27927a76598da2d
Afraz33 <afraz3301@gmail.com>
2024-03-21 22:08:53 +0500

    Files Setup

---
+++ main.py
@@ -0,0 +1,23 @@
+class mlops:
+    def __init__(self, totalStudents):
+        self.totalStudents = totalStudents
+
+    def getTotalStudents(self):
+        return self.totalStudents
+
+    def addStudents(self):
+        self.totalStudents = self.totalStudents+1
+
+    def removeStudent(self):
+        self.totalStudents = self.totalStudents - 1
+
+    def getClassName(self):
+        return "MLOPS (CS-B)"
+
+
+# random comment 5 to test builds
+mlops_class = mlops(5)
+mlops_class.addStudents()
+mlops_class.removeStudent()
+print(mlops_class.getTotalStudents())
+print(mlops_class.getClassName())
